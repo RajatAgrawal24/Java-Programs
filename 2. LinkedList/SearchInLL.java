35c424cea868a91ddaf88d0b538e047e4aea861c
RajatAgrawal <rag7731@gmail.com>
2025-09-05 19:04:02 +0530

    Added gitignore File

---
+++ 2. LinkedList/SearchInLL.java
@@ -0,0 +1,137 @@
+class LinkedList {
+    // Construction of LinkedList
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    // Add First
+    public void addFirst(int data) { // O(1)
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Add Last
+    public void addLast(int data) { // O(1)
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    // Add in middle
+    public void add(int idx, int data) { // O(n)
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    // Remove First
+    public int removeFirst() {
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        } else {
+            int val = head.data;
+            head = head.next;
+            size--;
+            return val;
+        }
+    }
+
+    // Remove Last
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        } else {
+            Node prev = head;
+            for (int i = 0; i < size - 2; i++) {
+                prev = prev.next;
+            }
+
+            int val = prev.next.data;
+            prev.next = null;
+            tail = prev;
+            size--;
+            return val;
+        }
+    }
+
+    // Print a LL
+    public void printLL() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // Get size of LL
+    public int getSize() {
+        return size;
+    }
+}
+
+public class SearchInLL {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+        ll.addLast(3);
+        ll.removeFirst();
+        ll.removeLast();
+
+        ll.printLL();
+        System.out.println("Size: " + ll.getSize());
+    }
+}
